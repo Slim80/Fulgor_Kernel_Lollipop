d85f2bff08bcdd2416705e8c3084da8630a765d1
Slim80 <bestboss1980@gmail.com>
2015-05-18 12:49:53 +0200

    Fix max VDD value

--- arch/arm/mach-msm/clock-krait-8974.c
+++ arch/arm/mach-msm/clock-krait-8974.c
@@ -606,7 +606,7 @@ module_param(pvs_config_ver, uint, S_IRUGO);
 #ifdef CONFIG_CPU_VOLTAGE_TABLE
 
 #define CPU_VDD_MIN	 650
-#define CPU_VDD_MAX	1450
+#define CPU_VDD_MAX	1300
 
 extern bool is_used_by_scaling(unsigned int freq);
 
