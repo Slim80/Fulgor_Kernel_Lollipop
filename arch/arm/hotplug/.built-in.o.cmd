4f2831f5a7ee43c62d91b0926fd9aad6ef6a1ab0
Slim80 <bestboss1980@gmail.com>
2015-06-19 23:26:55 +0200

    Added Intelli_Plug [Faux123]

---
+++ arch/arm/hotplug/.built-in.o.cmd
@@ -0,0 +1,1 @@
+cmd_arch/arm/hotplug/built-in.o :=  /home/slim80/Scrivania/kernel/Compilatori/linaro_a15_4.9.3-2015.03/bin/arm-eabi-ld -EL    -r -o arch/arm/hotplug/built-in.o arch/arm/hotplug/intelli_plug.o 
